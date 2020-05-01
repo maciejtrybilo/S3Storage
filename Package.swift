0bb90e32f3eeb626d125a76232ed671c78744325
Maciej Trybiło <maciektr@gmail.com>
2020-05-01 17:47:58 +0100

    Updated for Swift 5.2

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.2
 
 import PackageDescription
 
@@ -8,8 +8,8 @@ let package = Package(
         .library(name: "S3Storage", targets: ["S3Storage"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/LiveUI/S3.git", from: "3.0.0-rc2"),
-        .package(url: "https://github.com/skelpo/Storage.git", from: "0.1.0")
+        .package(url: "https://github.com/maciejtrybilo/S3.git", .branch("vapor3")),
+        .package(url: "https://github.com/maciejtrybilo/Storage.git", .branch("vapor3"))
     ],
     targets: [
         .target(name: "S3Storage", dependencies: ["Storage", "S3"]),
